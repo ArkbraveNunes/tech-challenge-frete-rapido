80938fbda429911401d8f02a7bc69f7d0769de5b
Jean <jeanclaudionunessantosnunes@gmail.com>
2024-03-29 16:04:19 -0300

    feat(INFRA): ✨ infra | create getMetrics repository method

--- src/infra/repository/simulation.repository.ts
+++ src/infra/repository/simulation.repository.ts
@@ -1,5 +1,11 @@
+import { PipelineStage } from 'mongoose';
+
 import { ILogger, LoggerService } from '@common/logger';
-import { ISimulationContract } from '@domain/contract';
+import {
+  GetMetricsInput,
+  GetMetricsOutput,
+  ISimulationContract,
+} from '@domain/contract';
 import { SimulationEntity } from '@domain/entity';
 import { simulationModel } from '@infra/schema';
 
@@ -11,6 +17,71 @@ export class SimulationRepository implements ISimulationContract {
     this.logger = new LoggerService(SimulationRepository.name);
     this._model = simulationModel;
   }
+
+  async getMetrics(
+    getMetricsInput?: GetMetricsInput,
+  ): Promise<GetMetricsOutput> {
+    try {
+      const baseQuery: PipelineStage[] = [
+        {
+          $sort: {
+            _id: -1,
+          },
+        },
+      ];
+
+      if (getMetricsInput?.skip) {
+        baseQuery.push({
+          $skip: getMetricsInput.skip,
+        });
+      }
+      if (getMetricsInput?.limit) {
+        baseQuery.push({
+          $limit: getMetricsInput.limit,
+        });
+      }
+
+      return await this._model.aggregate([
+        ...baseQuery,
+        {
+          $group: {
+            _id: '$name',
+            totalSimulations: {
+              $sum: 1,
+            },
+            totalPrice: {
+              $sum: '$price',
+            },
+            mediaPrice: {
+              $avg: '$price',
+            },
+            minPrice: {
+              $min: '$price',
+            },
+            maxPrice: {
+              $max: '$price',
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            name: '$_id',
+            totalSimulations: 1,
+            totalPrice: { $trunc: ['$totalPrice', 1] },
+            mediaPrice: { $trunc: ['$mediaPrice', 1] },
+            minPrice: 1,
+            maxPrice: 1,
+          },
+        },
+      ]);
+    } catch (error: any) {
+      this.logger.error({
+        msg: `Error in method ${this.getMetrics.name}`,
+        error,
+      });
+    }
+  }
   async createMany(simulationList: SimulationEntity[]): Promise<void> {
     try {
       await this._model.insertMany(
@@ -20,7 +91,10 @@ export class SimulationRepository implements ISimulationContract {
         })),
       );
     } catch (error: any) {
-      this.logger.error({ msg: 'Error in createMany simulations', error });
+      this.logger.error({
+        msg: `Error in method ${this.createMany.name}`,
+        error,
+      });
     }
   }
 }
